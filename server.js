136aacef7de5a83761234a139b6070da08e84ba7
shiza1aas <ahmsjahan@gmail.com>
2017-11-18 17:06:36 +0530

    Added changes for mongoose connection.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var config = require('./config');
 
 var app = express();
 
@@ -9,22 +11,42 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+var dbUrl = config.dbUrl;
+
+//a model of mongodb
+
+var Message = mongoose.model('Message',{
+	name: String,
+	message: String
+});
+
 //get requests
 
-var messages = [
-	{"name":"Khan","message":"Hello!"}, 
-]
+// var messages = [
+// 	{"name":"Khan","message":"Hello!"}, 
+// ]
 
 app.get('/messages', (req,res) => {
-	res.send(messages);
+	Message.find({},(err,messages)=>{
+		res.send(messages);
+	})
 })
 
 app.post('/messages', (req,res) => {
 	// console.log(req.body);
+	var message = new Message(req.body);
 
-	messages.push(req.body);
-	io.emit('message',req.body);
-	res.sendStatus(200);
+	message.save((err)=>{
+		if(err)
+			req.sendStatus(500);
+		// messages.push(req.body);
+		io.emit('message',req.body);
+		res.sendStatus(200);
+	})
+
+})
+mongoose.connect(dbUrl,{useMongoClient:true},(err)=>{
+	console.log("connected to mongodb",err);
 })
 
 io.on('connection',(socket) =>{
